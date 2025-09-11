1fa7f836cd4b790ff4567e11fec7cbdf2691f89e
Iporã Possantti <61351163+iporepos@users.noreply.github.com>
2025-09-11 15:34:18 -0300

    Create __init_.py

---
+++ src/copyme/mypackage/__init_.py
@@ -0,0 +1,33 @@
+"""  
+{Short package description (1-3 sentences)}  
+todo docstring  
+  
+Features  
+--------  
+todo docstring  
+  
+ - {feature 1} 
+ - {feature 2} 
+ - {feature 3} 
+ - {etc} 
+ 
+Overview  
+--------  
+todo docstring  
+{Overview description}  
+  
+Examples  
+--------  
+todo docstring  
+{Examples in rST} 
+
+Print a message
+
+.. code-block:: python
+
+    # print message
+    print("Hello world!")
+    # [Output] >> 'Hello world!'
+
+
+"""  
