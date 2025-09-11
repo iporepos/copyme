ce91b0527c2f394236cdfeed871030a5bba092f0
Iporã Possantti <61351163+iporepos@users.noreply.github.com>
2025-09-11 12:12:06 -0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,135 @@
+# SPHINX CONFIGURATION FILE
+# #######################################################################
+# This is a simple template for the modern configuration file for sphin
 
+# INSTRUCTIONS
+# ***********************************************************************
+# To adjust this to your project, you may want to:
+# 1) look for '[CHANGE THIS]' for mandatory modifications
+# 2) look for '[CHECK THIS]' for possible modifications
+# 3) look for '[EXAMPLE]' for simple examples (comment or uncomment it if needed)
+# 3) look for '[ADD MORE IF NEDDED]' for possible extra features
+
+# For the full list of built-in 
+# configuration values, see the documentation:  
+# https://www.sphinx-doc.org/en/master/usage/configuration.html  
+
+
+# REPO LAYOUT
+# ***********************************************************************
+import os  
+import sys  
+"""  
+for src layout (default)
+myrepo/  
+├── src/  
+│   └── myrepo/  
+│        ├── __init__.py  
+│        └── module.py  
+└── docs/   
+use >> sys.path.insert(0, os.path.abspath("../src")) 
+
+for flat layout 
+myrepo/  
+├──myrepo/  
+│   ├── __init__.py  
+│   └── module.py  
+└── docs/  
+use >> sys.path.insert(0, os.path.abspath(".."))  
+ 
+"""  
+sys.path.insert(0, os.path.abspath("../src")) # <-- [CHECK THIS] src layout  
+# sys.path.insert(0, os.path.abspath(".."))   # <-- [CHECK THIS] flat layout
+
+
+# REPO PROJECT INFO
+# ***********************************************************************
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information  
+
+project = '{name}' # todo fill  
+copyright = '{year}, {author}'  # todo fill  
+author = '{author}'  # todo fill  
+release = '{version}'  # todo fill  
+
+
+
+# GENERAL CONFIGS
+# ***********************************************************************
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration  
+  
+# Extensions  
+extensions = [  
+    # built-in extensions  
+    "sphinx.ext.duration",  
+    "sphinx.ext.doctest",  
+    "sphinx.ext.autodoc",  
+    "sphinx.ext.autosummary",  
+    "sphinx.ext.viewcode",  
+    "sphinx.ext.intersphinx",  
+    # extra extensions  
+    # -- nice copy button for codeblocks 
+    'sphinx_copybutton' # todo `python -m pip install sphinx-copybutton`  
+]  
+  
+# Ignore external dependencies  
+autodoc_mock_imports = [  
+    'numpy',  
+    'pandas',  
+    # ... keep adding as new dependencies arise  
+]  
+
+autodoc_member_order = 'bysource'  
+  
+# Exclude the __dict__, __weakref__, and __module__ attributes from being documented  
+exclude_members = [
+	'__dict__', 
+	'__weakref__', 
+	'__module__', 
+	'__str__'
+]  
+# Configure autodoc options  
+autodoc_default_options = {  
+    'members': True,  
+    'undoc-members': False,  
+    'private-members': True,  
+    'special-members': True,  
+    'show-inheritance': True,  
+    'exclude-members': ','.join(exclude_members)  
+}  
+
+templates_path = ['_templates']  
+exclude_patterns = [
+	'_build',
+	'Thumbs.db',
+	'.DS_Store'
+]  
+
+
+
+# HTML AND THEMES
+# ***********************************************************************
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output  
+"""  
+Some options for themes  
+  
+Built-in:  
+>> html_theme = "classic"  
+>> html_theme = "alabaster"  
+>> html_theme = "bizstyle"  
+  
+External (requires installation): 
+
+# PRO TIP: list external themes in pyproject.toml docs dependencies for automatic installation
+
+# by: `python -m pip install sphinx-rtd-theme`   
+>> html_theme = "sphinx_rtd_theme" 
+
+# by: `python -m pip install furo`
+>> html_theme = "furo"  
+ 
+# by: `python -m pip install pydata-sphinx-theme`  
+>> html_theme = "pydata_sphinx_theme" 
+  
+"""  
+html_theme = "alabaster"  
+html_static_path = ['_static']
